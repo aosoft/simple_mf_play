5fdee1a1b23d20b6929a5b7a137d54f5d59d7d5c
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2023-07-16 19:44:56 +0900

    WIP

--- cpp/window_message_queue.cpp
+++ cpp/window_message_queue.cpp
@@ -1,5 +1,33 @@
 #include "window_message_queue.h"
 
+window_message_queue::window_message_queue()
+    : _hwnd(nullptr)
+{
+}
+
+window_message_queue::~window_message_queue() noexcept
+{
+    detach();
+}
+
+void window_message_queue::attach(HWND hwnd)
+{
+    if (_hwnd != nullptr) {
+        return;
+    }
+    if (SubclassWindow(hwnd)) {
+        _hwnd = hwnd;
+    }
+}
+
+void window_message_queue::detach()
+{
+    if (_hwnd == nullptr) {
+        return;
+    }
+    UnsubclassWindow();
+}
+
 void window_message_queue::push(std::function<void()> fn)
 {
     {
