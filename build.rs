ef047492f48177d80104880c5e13451c7db9d209
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2023-07-20 00:57:36 +0900

    Add vtable_generation

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("cpp/mfplay.h")
         .clang_args(["-xc++"])
+        .vtable_generation(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
